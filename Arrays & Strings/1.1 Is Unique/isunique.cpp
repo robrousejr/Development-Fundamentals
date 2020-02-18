c9dd95df2b65f071668f34b8de30ddc34a5a5662
Main Desktop <robrousejr@gmail.com>
2020-02-17 22:18:06 -0500

    Finish isUnique

--- Arrays & Strings/1.1 Is Unique/isunique.cpp
+++ Arrays & Strings/1.1 Is Unique/isunique.cpp
@@ -4,22 +4,42 @@ cannot use additional data structures?
 */
 #include <iostream>
 #include <string>
-#include "../Hash Table/hashtable.h" // Hash table code
 
 using std::string;
+using std::cout;
+using std::endl;
 
-// Trying to implement with hash table
+// has all unique characters?
 bool isUnique(string word) {
-    
-}
 
-// No data structures implemented
-bool isUniqueNoDataStructures(string word)
-{
+    // Iterate through characters in word
+    for(string::iterator it = word.begin(); it != word.end(); it++)
+    {
+        char letter = *it; // current letter
 
+        // iterate from current letter on
+        for(string::iterator subIt = it + 1; subIt != word.end(); subIt++)
+        {
+            cout << "Comparing " << letter << " with " << *subIt << endl;
+
+            // if characters are equal, false
+            if(*subIt == letter){
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 int main(){
+    string test = "alpsun";
+    bool unique = isUnique(test);
+
+    if(unique)
+        cout << "String '" << test << "' is unique\n";
+    else
+        cout << "String '" << test << "' is NOT unique\n";    
+    
 
     return 0;
 }
