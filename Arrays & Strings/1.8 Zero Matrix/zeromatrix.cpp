adebea3393e13be94ec9228601d4164d4a0b647c
Main Desktop <robrousejr@gmail.com>
2020-02-22 18:49:35 -0500

    Add zeroMatrix function

--- Arrays & Strings/1.8 Zero Matrix/zeromatrix.cpp
+++ Arrays & Strings/1.8 Zero Matrix/zeromatrix.cpp
@@ -5,8 +5,13 @@ column are set to 0.
 #include <iostream>
 #include "zeromatrix.h"
 
+using std::cout;
 
 int main(){
     int photo[5][4] = {{9,2,4,5}, {5,0,7,8}, {1,4,5,6}, {2,4,3,3}, {1,7,8,5}};
+    printArray(photo);
+    zeroMatrix(photo);
+
+    cout << "\nAfter zeroMatrix()\n";
     printArray(photo);
 }
--- Arrays & Strings/1.8 Zero Matrix/zeromatrix.h
+++ Arrays & Strings/1.8 Zero Matrix/zeromatrix.h
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <vector>
 
 using std::cout;
+using std::endl;
+using std::vector;
+using std::cerr;
 
 // Print 2D array to console
 template<typename T, size_t row, size_t col>
@@ -16,13 +20,48 @@ void printArray(T(&array)[row][col])
     }
 }
 
-
+// Changes row and column to all 0's where 0 is present
 template<typename T, size_t row, size_t col>
-void zeroMatrix(T(&array)[row][col])
+T zeroMatrix(T(&array)[row][col])
 {
-    /**
-     * 1. Find 0's and remember place
-     * 2. Make that row and column full of 0's
-     * Attempt this in-place if you can... 
-    */
+    // Holds rows/cols where 0's are found
+    vector<int> zeroRows = {};
+    vector<int> zeroCols = {};
+
+    // Go through matrix and find 0's
+    for(int x = 0; x < row; x++)
+    {  
+        for(int y = 0; y < col; y++)
+        {
+            // if zero, save row and column
+            if(array[x][y] == 0)
+            {
+                zeroRows.push_back(x); // Save row
+                zeroCols.push_back(y); // Save col
+            }
+        }
+    }
+
+    if(zeroRows.size() != zeroCols.size())
+    {
+        cerr << "ERROR: rows and columns saved aren't the same...";
+        return -1;
+    }
+
+    // Make row and column full of 0's
+    for(int i = 0; i < zeroCols.size(); i++)
+    {
+        int thisRow = zeroRows[i];
+        int thisCol = zeroCols[i];
+
+        for(int currRow = 0; currRow < row; currRow++)
+        {
+            array[currRow][thisCol] = 0;
+        }
+
+        for(int currCol = 0; currCol < col; currCol++)
+        {
+            array[thisRow][currCol] = 0;
+        }
+    }
 }
