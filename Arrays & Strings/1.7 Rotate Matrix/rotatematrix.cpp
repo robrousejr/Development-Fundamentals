09f2109dcb32dbd6f574b34dbb2c28dd1d4dd0dc
Main Desktop <robrousejr@gmail.com>
2020-02-22 01:11:07 -0500

    Create rotateRight function

--- Arrays & Strings/1.7 Rotate Matrix/rotatematrix.cpp
+++ Arrays & Strings/1.7 Rotate Matrix/rotatematrix.cpp
@@ -10,7 +10,10 @@ using std::endl;
 
 int main(){
 
-    int photo[3][4] = {{0,1,2,3}, {4,5,6,7}, {8,9,1,2}};
+    char photo[4][4] = {{'A','B','C','D'}, {'E','F','G','H'}, {'I','J','K','L'}, {'M','N','O','P'}};
+    printMatrix(photo);
+    cout << "\n\nRotating...\n";
+    rotateRight(photo);
     printMatrix(photo);
 
 }
--- Arrays & Strings/1.7 Rotate Matrix/rotatematrix.h
+++ Arrays & Strings/1.7 Rotate Matrix/rotatematrix.h
@@ -1,15 +1,47 @@
 #include <iostream>
+#include <vector>
 
 using std::cout;
 using std::endl;
+using std::vector;
 
-// Rotates matrix by 90 degrees
-void rotateMatrix(int matrix[3][4]) {
+// Prototypes
+void printMatrix(char[4][4]);
+
+// Rotates a matrix to the right
+void rotateRight(char matrix[4][4])
+{
+
+    char arr[16] = {};
+    char tmpCt = 0;
+
+    // Fill temporary array with correct order of values
+    for(int row = 3; row >= 0; row--)
+    {
+        for(int col = 0; col <= 3; col++)
+        {
+            arr[tmpCt] = matrix[row][col];
+            tmpCt++;
+        }
+    }
+    
+    tmpCt = 0; // reset temporary counter 
+
+    // Store new order of values in matrix
+    for(int col = 0; col <= 3; col++)
+    {
+        for(int row = 0; row <= 3; row++)
+        {
+            matrix[row][col] = arr[tmpCt];
+            tmpCt++;
+        }
+    }
 
 }
 
-void printMatrix(int photo[3][4]) {
-    for(int row = 0; row < 3; row++)
+// Output matrix to console
+void printMatrix(char photo[4][4]) {
+    for(int row = 0; row < 4; row++)
     {
         for(int col = 0; col < 4; col++)
         {
