c37a49edc21065dde87facc797e862bd53d0d58e
Main Desktop <robrousejr@gmail.com>
2020-02-28 22:47:54 -0500

    Fill linked list

--- Linked Lists/2.1 Remove Dups/removedups.cpp
+++ Linked Lists/2.1 Remove Dups/removedups.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include "removedups.h"
+#include "../Linked Lists/linkedlist.h"
 
 using std::cout;
 
 int main()
 {
-    
+    LinkedList a;
+
+    // Fill Linked List 
+    for(int i = 1; i <= 10; i++)
+    {
+        a.append(i);
+    }
+
+    a.printList();
 
     return 0;
 }
