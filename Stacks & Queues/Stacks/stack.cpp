6b7624d47311841abcdff5fd9229119d4e65ec56
Rob Rouse Jr <robrousejr@gmail.com>
2020-02-29 20:57:37 -0500

    Add printPopStack function

--- Stacks & Queues/Stacks/stack.cpp
+++ Stacks & Queues/Stacks/stack.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include "stack.h"
+#include <stack>
 
 using std::cout;
 using std::endl; 
+using std::stack;
 
 int main()
 {
-    
+    stack<int> a;
+    a.push(3);
+    a.push(5);
+    a.push(2);
+
+    printPopStack(a);
+
+
     return 0;
 }
--- Stacks & Queues/Stacks/stack.h
+++ Stacks & Queues/Stacks/stack.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <stack>
+#include <iostream>
+
+using std::stack;
+using std::cout;
+
+/**
+ * Pop out all elements on stack and print them out
+*/
+void printPopStack(stack<int> &a)
+{
+    while(!a.empty())
+    {
+        cout << " " << a.top(); 
+        a.pop();
+    }
+}
