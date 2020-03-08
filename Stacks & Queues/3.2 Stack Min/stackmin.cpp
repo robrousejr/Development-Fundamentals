10befc066428b1a336c5a54deda679cc00075733
Main Desktop <robrousejr@gmail.com>
2020-03-07 23:19:29 -0500

    Fill stack

--- Stacks & Queues/3.2 Stack Min/stackmin.cpp
+++ Stacks & Queues/3.2 Stack Min/stackmin.cpp
@@ -4,12 +4,21 @@ Find and return the minimum element in a stack
 
 #include <iostream>
 #include <stack>
+#include "../Stacks & Queues/stackqueue.h"
 
 using std::cout;
 using std::stack;
 
 int main()
 {
+    stack<int> a;
+
+    // Fill stack
+    for(int i = 0; i < 10; i++)
+    {
+        a.push(i);
+    }
+
 
     return 0;
 }
